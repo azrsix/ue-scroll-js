64eb4877b1bb81581e0f7e49374ea0b3dd92be55
azrsix <azrsix@gmail.com>
2018-10-05 02:30:37 +0900

    Update docs/

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -1,15 +1,18 @@
+const path = require('path');
 const MinifyPlugin = require('babel-minify-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const contents = ['index', 'docs'];
+const pkg = require('../package.json');
 
+const siteUrl = pkg.homepage;
+const langs = ['en', 'ja'];
+const contents = ['index', 'docs'];
 const config = {
-  entry: {
-    'js/app.min.js': './src/js/app.js',
-    'css/style.min.css': './src/css/style.css'
-  },
+  mode: 'production',
+  entry: ['./src/js/app.js', './src/css/style.css'],
   output: {
-    filename: '[name]'
+    path: path.resolve(__dirname),
+    filename: 'js/app.min.js'
   },
   module: {
     rules: [{
@@ -18,22 +21,21 @@ const config = {
       use: [{
         loader: 'babel-loader',
         options: {
-          presets: ['es2015']
+          presets: ['@babel/preset-env']
         }
       }]
     }, {
       test: /\.css$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [{
-          loader: 'css-loader',
-          options: {
-            importLoaders: 1
-          }
-        }, {
-          loader: 'postcss-loader'
-        }]
-      })
+      use: [{
+        loader: MiniCssExtractPlugin.loader
+      }, {
+        loader: 'css-loader',
+        options: {
+          importLoaders: 1
+        }
+      }, {
+        loader: 'postcss-loader'
+      }]
     }, {
       test: /\.pug$/,
       loader: 'pug-loader'
@@ -41,22 +43,39 @@ const config = {
   },
   plugins: [
     new MinifyPlugin(),
-    new ExtractTextPlugin('[name]'),
-  ].concat(contents.map((name) => {
-    return new HtmlWebpackPlugin({
-      filename: `${name}.html`,
-      template: `src/${name}.pug`,
-      inject: false,
-      minify: {
-        caseSensitive: true,
-        collapseInlineTagWhitespace: true,
-        collapseWhitespace: true,
-        minifyCSS: true,
-        minifyJS: true,
-        removeComments: true
-      }
+    new MiniCssExtractPlugin({
+      filename: 'css/style.min.css'
+    })
+  ].concat(langs.map((lang) => {
+    const dir = (lang === 'en')
+      ? { file: '.',  link: '' }
+      : { file: lang, link: `${lang}/` };
+    const langFile = require(`./src/locales/${lang}.json`);
+    const relativePath = (lang === 'en') ? '' : '../';
+    return contents.map((name) => {
+      const indexOrOtherwise = (name === 'index') ? '' : `${name}.html`;
+      const permalink = `${siteUrl}${dir.link}${indexOrOtherwise}`;
+      return new HtmlWebpackPlugin({
+        filename: `${dir.file}/${name}.html`,
+        template: `src/${name}.pug`,
+        inject: false,
+        minify: {
+          caseSensitive: true,
+          collapseInlineTagWhitespace: true,
+          collapseWhitespace: true,
+          minifyCSS: true,
+          minifyJS: true,
+          removeComments: true
+        },
+        templateVars: {
+          lang: langFile,
+          relativePath : relativePath,
+          siteUrl: siteUrl,
+          permalink: permalink,
+        }
+      })
     });
-  }))
+  }).reduce((arr, x) => arr.concat(x), []))
 };
 
 module.exports = config;
